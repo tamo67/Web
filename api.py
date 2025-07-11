2ec801af8efea8cfe0ee99307f3e7c0fd635483e
Adrian Ciubotaru <adrianceo27@gmail.com>
2025-07-11 09:58:21 -0400

    A

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
+import os
 from amadeus import Client, ResponseError
 
 amadeus = Client(
-    client_id='NAHa5Gwxsia2CSfJKod3yjdDYpCXJwMC',
-    client_secret='LXwncGgeKIIiJ9mt'
+    client_id=os.environ.get("AMADEUS_ID"),
+    client_secret=os.environ.get("AMADEUS_SECRET")
 )
-
 def get_flight_data_fixed(origin, destination, departure_date):
     try:
         response = amadeus.shopping.flight_offers_search.get(
